8aea46c90bd976271669b5ba4212a5c46c744ab7
Alex <alex@alex-dev.(none)>
2011-01-19 18:39:27 -0600

    last changes minos changes on interfaces and general clean

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :states
   map.resources :patients, :has_many => [:psychologies]
   map.resource :navigator
-  map.root :controller => "navigators"
+  map.root :controller => "user_sessions", :action => "new"
   #users routes
   map.login '/login', :controller => 'user_sessions', :action => 'new'
   map.logout '/logout', :controller => 'user_sessions', :action => 'destroy'
