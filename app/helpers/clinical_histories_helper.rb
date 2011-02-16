944c94d05e6d5219f82ebf56ecc7ed79a0e756c9
Alex S <alexs1010@gmail.com>
2011-02-16 12:47:47 -0600

    add restriction for clinical history

--- app/helpers/clinical_histories_helper.rb
+++ app/helpers/clinical_histories_helper.rb
@@ -1,2 +1,23 @@
 module ClinicalHistoriesHelper
+  def feed(valor)
+    if valor.nil?
+      "No seleccionado"
+    elsif valor == 16
+      "Suficiente"
+    elsif valor == 17
+      "Pobre"
+    elsif valor == 18
+      "Adecuada"
+    elsif valor == 19
+      "Excesivo"
+    else
+      "Dieta no habitual"
+    end
+  end
+
+  def checked_value(valor)
+    if valor == false
+      image_tag("checked.png")
+    end
+  end
 end
--- app/models/clinical_history.rb
+++ app/models/clinical_history.rb
@@ -1,4 +1,6 @@
 class ClinicalHistory < ActiveRecord::Base
   has_many :past_pregnants
   accepts_nested_attributes_for :past_pregnants, :reject_if => :all_blank
+
+  validates_uniqueness_of :patient_id
 end
