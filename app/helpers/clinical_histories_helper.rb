a858ede93df1d08d2860b50d88e9abbe16999aea
Luis Silva <luis@luis-dev.(none)>
2011-03-23 13:47:06 -0600

    se terminaron cambios historia clinica para impresion

--- app/helpers/clinical_histories_helper.rb
+++ app/helpers/clinical_histories_helper.rb
@@ -39,4 +39,77 @@ module ClinicalHistoriesHelper
       image_tag("checked.png")
     end
   end
+
+  def dilated_value(valor)
+    if valor.nil?
+      "No seleccionado"
+    elsif valor == 1
+      "0-2"
+    elsif valor == 2
+      "3-4"
+    elsif valor == 3
+      "5-6"
+    elsif valor == 4
+      "+7"
+    end
+  end
+
+   def longitud_value(valor)
+    if valor.nil?
+      "No seleccionado"
+    elsif valor == 1
+      "largo"
+    elsif valor == 2
+      "corto"
+    elsif valor == 3
+      "semiborrado"
+    end
+   end
+
+  def consistence_value(valor)
+    if valor.nil?
+      "No seleccionado"
+    elsif valor == 1
+      "resistente"
+    elsif valor == 2
+      "intermedio"
+    elsif valor == 3
+      "blando"
+    end
+  end
+
+  def lucorre_value(valor)
+    if valor.nil?
+      "No seleccionado"
+    elsif valor == 1
+      "escaso"
+    elsif valor == 2
+      "moderado"
+    elsif valor == 3
+      "abundante"
+    end
+  end
+
+  def sangre_value(valor)
+    if valor.nil?
+      "No seleccionado"
+    elsif valor == 1
+      "fresco"
+    elsif valor == 2
+      "antiguio"
+    end
+  end
+
+  def fondo_value(valor)
+    if valor.nil?
+      "No seleccionado"
+    elsif valor == 1
+      "libro"
+    elsif valor == 2
+      "abombados"
+    end
+  end
+
+  
+
 end
